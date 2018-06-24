3d0b81c948ebb2ea3532954a9aa96eed581d4a3f
César Collé <cesar.colle@gmail.com>
2018-06-24 17:06:55 +0200

    Add the template integration

--- src/main/java/com/github/cesarcolle/sequana/Main.java
+++ src/main/java/com/github/cesarcolle/sequana/Main.java
@@ -18,8 +18,8 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
         CharStream charStream = getCharStream(args);
+        Model sequanaModel = buildModel(charStream);
     }
 
 
--- src/main/java/com/github/cesarcolle/sequana/generator/SequanaGenerator.java
+++ src/main/java/com/github/cesarcolle/sequana/generator/SequanaGenerator.java
@@ -1,4 +1,50 @@
 package com.github.cesarcolle.sequana.generator;
 
-public class SequanaGenerator {
+import com.github.cesarcolle.sequana.model.Model;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SequanaGenerator implements Runnable {
+    private static String DIR_OUT_ARDUINO = "";
+    private static String DIR_OUT_SERVER = "";
+
+    private static String SEQUANA_SERVER_PATH = "src/main/resources/sequana-server/";
+    private static String SEQUANA_SERVER_BUILD = SEQUANA_SERVER_PATH + "build.sbt";
+    private static String SEQUANA_SERVER_WEB = SEQUANA_SERVER_PATH + "WebServer.scala";
+
+    // model and engine
+    private final VelocityEngine engine;
+    private final VelocityContext context;
+    private final  Model model;
+
+    public SequanaGenerator(Model model){
+        this.engine = new VelocityEngine();
+        this.context = new VelocityContext();
+        this.model = model;
+
+        createContext();
+    }
+
+    private void createContext(){
+        context.put("frequency", model.getFrequencies());
+        context.put("area", model.getAreas());
+        context.put("pipes", model.getPipes());
+        context.put("devices", model.getDevices());
+    }
+
+
+    @Override
+    public void run() {
+        Template templateServer = engine.getTemplate(SEQUANA_SERVER_WEB);
+        try {
+            templateServer.merge(context, new FileWriter("out.out"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
